e71705a7316e52cc4bb2bfd42d72ef2b853b0b5c
Roubert Edgar <roubertedgar@gmail.com>
2020-07-17 15:46:57 -0300

    Configures themes and styles

---
+++ app/src/main/java/com/favoriteplaces/location/details/LocationDetailsFragment.kt
@@ -0,0 +1,13 @@
+package com.favoriteplaces.location.details
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.favoriteplaces.R
+
+class LocationDetailsFragment : Fragment(R.layout.location_list_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
---
+++ app/src/main/java/com/favoriteplaces/location/list/LocationListFragment.kt
@@ -0,0 +1,7 @@
+package com.favoriteplaces.location.list
+
+import androidx.fragment.app.Fragment
+
+class LocationListFragment : Fragment() {
+
+}
