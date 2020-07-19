991b78435575de23d5f8e9d91326fc13b452461f
Roubert Edgar <roubertedgar@gmail.com>
2020-07-19 03:03:19 -0300

    Observes view instruction on location list fragment

--- app/src/main/java/com/favoriteplaces/location/list/LocationListFragment.kt
+++ app/src/main/java/com/favoriteplaces/location/list/LocationListFragment.kt
@@ -3,9 +3,12 @@ package com.favoriteplaces.location.list
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.downstairs.eatat.core.tools.Instruction
+import com.downstairs.eatat.core.tools.State
 import com.favoriteplaces.R
 import com.favoriteplaces.core.extensions.getCoreComponent
 import com.favoriteplaces.location.injection.DaggerLocationComponent
@@ -36,13 +39,30 @@ class LocationListFragment : Fragment(R.layout.location_list_fragment) {
     }
 
     private fun setupObservers() {
+        viewModel.viewInstruction.observe(viewLifecycleOwner, Observer { onInstructionChange(it) })
         viewModel.locationList.observe(viewLifecycleOwner, Observer { onLocationListResult(it) })
+    }
+
+    private fun onInstructionChange(instruction: Instruction) {
+        when (instruction) {
+            is State.Success -> toDefaultState()
+            is State.Loading -> toLoadingState()
+            is State.Failed -> toDefaultState()
+        }
     }
 
     private fun onLocationListResult(locationList: List<LocationUIModel>) {
         getLocationAdapter()?.also {
             it.submitList(locationList)
         }
+    }
+
+    private fun toDefaultState() {
+        locationListProgressBar.isVisible = false
+    }
+
+    private fun toLoadingState() {
+        locationListProgressBar.isVisible = true
     }
 
     private fun getLocationAdapter() = (locationRecyclerView.adapter as? LocationAdapter)
