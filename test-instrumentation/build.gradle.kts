37944248a40d2c07688f66f9ab006cf6d9139ef2
Roubert Edgar <roubert.edgar@gmail.com>
2022-06-06 10:02:23 -0300

    Clean build gradle

--- test-instrumentation/build.gradle.kts
+++ test-instrumentation/build.gradle.kts
@@ -4,22 +4,15 @@ plugins {
 }
 
 android {
-    compileSdk = 31
     defaultConfig {
-        minSdk = 23
-        targetSdk = 30
-
         testApplicationId = "com.favoriteplaces.instrumentation"
-        testInstrumentationRunnerArguments["class"] = "com.favoriteplaces.instrumentation.InstrumentationTestSuit"
+        testInstrumentationRunnerArguments["class"] =
+            "com.favoriteplaces.instrumentation.InstrumentationTestSuit"
     }
 }
 
 dependencies {
     localImplementation(project(":core:test"))
 
-//    implementation(Dependencies.Test.assertJ)
-//    implementation(Dependencies.Test.androidxCore)
-//    implementation(Dependencies.Test.runner)
-//    implementation(Dependencies.Test.espresso)
     implementation(Dependencies.Test.uiAutomator)
 }
