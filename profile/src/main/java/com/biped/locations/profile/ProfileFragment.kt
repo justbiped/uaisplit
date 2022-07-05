9d42a6fb8e86daa310effb8a33fdbae3623f938a
Roubert Edgar <roubertedgar@gmail.com>
2022-07-05 10:01:36 -0300

    Create dimens files

--- profile/src/main/java/com/biped/locations/profile/ProfileFragment.kt
+++ profile/src/main/java/com/biped/locations/profile/ProfileFragment.kt
@@ -4,24 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import coil.compose.rememberAsyncImagePainter
 import com.biped.locations.profile.ui.theme.LocationsandroidTheme
 
 class ProfileFragment : Fragment() {
@@ -34,41 +18,14 @@ class ProfileFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 LocationsandroidTheme {
-                    ProfileView()
+                    ProfileView(
+                        UserUiModel(
+                            "Edgar",
+                            "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg"
+                        )
+                    )
                 }
             }
         }
     }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun ProfileView() {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 16.dp, start = 16.dp, end = 16.dp)
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
-        ){
-            ProfileHeader()
-        }
-    }
-}
-
-@Composable
-fun ProfileHeader() {
-    Image(
-        painter = rememberAsyncImagePainter(
-            model = "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg",
-            placeholder = painterResource(id = R.drawable.ic_profile_on)
-        ),
-        modifier = Modifier
-            .clip(CircleShape)
-            .size(100.dp),
-        contentScale = ContentScale.Crop,
-        contentDescription = ""
-    )
 }
---
+++ profile/src/main/java/com/biped/locations/profile/ProfileFragmentView.kt
@@ -0,0 +1,73 @@
+package com.biped.locations.profile
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import coil.compose.rememberAsyncImagePainter
+import com.biped.locations.profile.ui.theme.Dimens
+import com.biped.locations.profile.ui.theme.SmallSpacer
+import com.biped.locations.profile.ui.theme.panel
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProfileView(@PreviewParameter(UserPreview::class) user: UserUiModel) {
+    Column(
+        modifier = Modifier.panel()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        ) {
+            ProfileHeader(user)
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(3f)
+        ) {
+        }
+    }
+}
+
+@Composable
+fun ProfileHeader(user: UserUiModel) {
+    val profileImagePainter = rememberAsyncImagePainter(
+        model = user.imageUrl,
+        placeholder = painterResource(id = R.drawable.ic_profile_on)
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = Dimens.small),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = profileImagePainter,
+            modifier = Modifier
+                .weight(1f)
+                .aspectRatio(1f)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop,
+            contentDescription = ""
+        )
+        SmallSpacer()
+        Text(text = user.name)
+    }
+}
+
+class UserPreview : PreviewParameterProvider<UserUiModel> {
+    override val values = sequenceOf(UserUiModel("Edgar"))
+}
