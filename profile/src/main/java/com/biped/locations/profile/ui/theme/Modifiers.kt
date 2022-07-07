0d5f3c400860b3862b2a9f69026a88976ed50949
Roubert Edgar <roubertedgar@gmail.com>
2022-07-07 01:10:36 -0300

    Use dimens normal from dimens xml

--- profile/src/main/java/com/biped/locations/profile/ui/theme/Modifiers.kt
+++ profile/src/main/java/com/biped/locations/profile/ui/theme/Modifiers.kt
@@ -3,9 +3,7 @@ package com.biped.locations.profile.ui.theme
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
-import androidx.compose.ui.res.dimensionResource
-import com.biped.locations.profile.R
 
 fun Modifier.panel() = Modifier.composed {
-    padding(horizontal = dimensionResource(R.dimen.normal))
+    padding(horizontal = Dimens.normal)
 }
