72f56870a7a71d9dc928594144e9b913a9426419
Roubert Edgar <roubertedgar@gmail.com>
2022-10-16 14:54:31 -0300

    Uses nav destination to create statement directions

--- features/statement/src/main/java/biped/works/tosplit/statement/StatementNavGraph.kt
+++ features/statement/src/main/java/biped/works/tosplit/statement/StatementNavGraph.kt
@@ -1,16 +1,14 @@
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import biped.works.tosplit.statement.ui.StatementScreen
+import com.favoriteplaces.core.compose.NavDestination
+import com.favoriteplaces.core.compose.composable
 
-sealed class StatementDirections(
-    val route: String, arguments: List<NamedNavArgument> = emptyList()
-) {
-    object Graph : StatementDirections("statement_graph_route")
-    object Statement: StatementDirections("statement_route")
+sealed interface StatementDirections {
+    object Graph : NavDestination("statement_graph_route")
+    object Statement : NavDestination("statement_route")
 }
 
 fun NavGraphBuilder.statementGraph(navController: NavController) {
@@ -18,7 +16,7 @@ fun NavGraphBuilder.statementGraph(navController: NavController) {
         startDestination = StatementDirections.Statement.route,
         route = StatementDirections.Graph.route
     ) {
-        composable(StatementDirections.Statement.route) {
+        composable(StatementDirections.Statement) {
             StatementScreen(viewModel = hiltViewModel())
         }
     }
