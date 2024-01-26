c844e7f4b157689e05f0b6c905fbb651e014ccb5
Roubert Edgar <roubertedgar@gmail.com>
2024-01-26 17:07:59 -0300

    Fix gradle plugins

--- buildSrc/src/main/kotlin/biped/works/plugins/github/BumpVersionTask.kt
+++ buildSrc/src/main/kotlin/biped/works/plugins/github/BumpVersionTask.kt
@@ -33,17 +33,3 @@ open class CommitTask : Exec() {
         return Version(major.toInt(), minor.toInt(), patch.toInt())
     }
 }
-
-data class Version(
-    val major: Int,
-    val minor: Int,
-    val patch: Int
-) {
-    fun incrementMinor(value: Int = 1) = copy(minor = minor + value)
-
-    override fun toString() = "$major.$minor.$patch"
-
-    fun inLines(): List<String> {
-        return "version.major=$major\nversion.minor=$minor\nversion.patch=$patch".split("\n")
-    }
-}
