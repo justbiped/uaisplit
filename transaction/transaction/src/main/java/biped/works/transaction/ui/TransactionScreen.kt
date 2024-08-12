be4b65b935ec1caca1c0bc976e23278b7459a51c
Roubert Edgar <roubertedgar@gmail.com>
2024-08-12 18:59:57 -0300

    Use secondary color for focused text fields

--- transaction/transaction/src/main/java/biped/works/transaction/ui/TransactionScreen.kt
+++ transaction/transaction/src/main/java/biped/works/transaction/ui/TransactionScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -14,6 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import biped.works.compose.collectWithLifecycle
 import biped.works.transaction.R
@@ -41,12 +44,23 @@ internal fun TransactionScreen(viewModel: TransactionViewModel) {
         }
     }
 
-    if (state.isLoading) LoadingPanel()
-    else TransactionPanel(state.uiModel)
+    if (state.isLoading) {
+        LoadingPanel()
+    } else {
+        TransactionPanel(
+            state.uiModel,
+            onFormChange = {}
+        )
+    }
 }
 
 @Composable
-fun TransactionPanel(uiModel: TransactionUiModel) {
+fun TransactionPanel(
+    uiModel: TransactionUiModel,
+    onFormChange: () -> Unit = {}
+) {
+    var form by remember { mutableStateOf(uiModel) }
+
     Column(
         Modifier
             .fillMaxWidth()
@@ -55,24 +69,36 @@ fun TransactionPanel(uiModel: TransactionUiModel) {
         TopAppbar({}, {})
         SmallSpacer()
         Column(modifier = Modifier.padding(horizontal = Dimens.small)) {
+
+            val editTextColors = OutlinedTextFieldDefaults.colors()
+                .copy(
+                    focusedLabelColor = MaterialTheme.colorScheme.secondary,
+                    focusedIndicatorColor = MaterialTheme.colorScheme.secondary
+                )
+
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
-                value = uiModel.name,
-                label = { Text(stringResource(R.string.transaction_name)) },
-                onValueChange = {})
+                value = form.name,
+                label = { Text(text = stringResource(R.string.transaction_name)) },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                colors = editTextColors,
+                onValueChange = { form = form.copy(name = it) })
             SmallSpacer()
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
-                value = uiModel.description,
+                value = form.description,
                 label = { Text(stringResource(R.string.transaction_description)) },
-                onValueChange = {})
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                colors = editTextColors,
+                onValueChange = { form = form.copy(description = it) })
             SmallSpacer()
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
-                value = uiModel.amount.toString(),
+                value = form.amount.toString(),
                 prefix = { Text(uiModel.currency) },
                 label = { Text(stringResource(R.string.transaction_amount)) },
-                onValueChange = {})
+                colors = editTextColors,
+                onValueChange = { })
         }
     }
 }
