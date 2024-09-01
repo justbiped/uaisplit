bf211676bfc6be481448d359073a1f076fb0a68b
Roubert Edgar <roubertedgar@gmail.com>
2024-09-01 20:01:31 -0300

    Configure kover and test plugins

---
+++ buildSrc/src/main/kotlin/biped/works/plugins/TestPlugin.kt
@@ -0,0 +1,65 @@
+package biped.works.plugins
+
+import AndroidExtension
+import android
+import devImplementation
+import id
+import libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.findByType
+import testImplementation
+
+val noAndroidPluginException: Throwable
+    get() = Throwable("Make sure that you have declared the Application or Library plugin")
+
+class ComposePlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        project.allprojects {
+            project.extensions.findByType<AndroidExtension>() ?: noAndroidPluginException
+
+            project.plugins.apply(libs.plugins.compose.compiler.id)
+
+            android {
+                buildFeatures.apply {
+                    compose = true
+                }
+            }
+
+            dependencies {
+                //implementation(catalog.library("compose.bom"))
+                implementation(libs.compose.foundation)
+                implementation(libs.compose.ui)
+                implementation(libs.compose.icons)
+                implementation(libs.compose.iconsExtended)
+                implementation(libs.compose.animation)
+                implementation(libs.compose.hilt)
+                implementation(libs.compose.coil)
+
+                implementation(libs.compose.material)
+                implementation(libs.compose.material.window)
+                implementation(libs.compose.material.navigation)
+
+                // Navigation
+                implementation(libs.compose.navigation)
+
+                // Preview
+                implementation(libs.compose.ui.tooling.preview)
+                devImplementation(libs.compose.ui.tooling)
+
+                // Test
+                testImplementation(libs.compose.test.junit)
+                devImplementation(libs.compose.test.manifest)
+
+                // Biped Compose Foundation
+                implementation(project(":foundation:compose"))
+            }
+        }
+    }
+}
+
+private fun DependencyHandlerScope.implementation(dependencyNotation: Any) {
+    add("implementation", dependencyNotation)
+}
