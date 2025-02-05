3736b946922a600ef0a6dbd7330f9863c109515d
Roubert Edgar <roubertedgar@gmail.com>
2025-02-05 02:16:19 -0300

    Simplify home navigation states

--- app/src/main/java/biped/works/locations/home/ui/HomeViewModel.kt
+++ app/src/main/java/biped/works/locations/home/ui/HomeViewModel.kt
@@ -1,19 +1,13 @@
 package biped.works.locations.home.ui
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import biped.works.coroutines.asUiState
 import biped.works.coroutines.mutableEventFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import kotlinx.coroutines.flow.MutableStateFlow
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
 ) : ViewModel() {
-
-    private val _uiState = MutableStateFlow(HomeUiState)
-    val uiState = _uiState.asUiState(viewModelScope)
 
     val uiEvent = mutableEventFlow<HomeEvent>()
 
--- automation/build.gradle.kts
+++ automation/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     apply(libs.plugins.java)
     apply(libs.plugins.kotlin.jvm)
@@ -7,6 +9,12 @@ plugins {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
